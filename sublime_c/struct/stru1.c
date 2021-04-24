c97611d46acb81695dd5bb54a8754ef06181ddc0
zhang <654014730@qq.com>
2021-04-24 22:21:47 +0800

    struct

--- sublime_c/struct/stru1.c
+++ sublime_c/struct/stru1.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
 
-struct user{
+int main(){
+    struct 
+    {
+        char *name;
+        int num;
+        int age;
+        char group;
+        float score;
+    } stu1;
+
+    stu1.name = "jerry";
+    stu1.num = 12;
+    stu1.age = 24;
+    stu1.group = 'A';
+    stu1.score = 59.9;
+
+    printf("%s的学号是%d，年龄是%d，在%c组，成绩是%.1f\n",stu1.name,stu1.num,stu1.age,stu1.group,stu1.score);
     
-};
+    return 0;
+}
