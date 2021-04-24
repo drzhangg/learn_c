29fa4a0b5e7112a012ebcb52bd11e60d928725ab
zhang <654014730@qq.com>
2021-04-24 21:46:55 +0800

    c

---
+++ sublime_c/pointer/13.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    char *str[3]={
+        "c.biancheng.net",
+        "c语言中文网",
+        "c language"
+    };
+    printf("%s\n%s\n%s\n",str[0],str[1],str[2]);
+    return 0;
+}
